72ab72513087a0e519dcc7497f1df2ce42ddbfc3
Halbout Michael <michaelhalbout@gmail.com>
2017-09-28 08:39:31 +0200

    Update eibd.js

--- desktop/js/eibd.js
+++ desktop/js/eibd.js
@@ -253,31 +253,48 @@ $('body').on('change','.cmdAttr[data-l1key=type]',function() {
 	}
 });			
 $('body').on('change','.cmdAttr[data-l1key=subType]', function() {
+	var Dpt=$('.cmdAttr[data-l1key=configuration][data-l2key=KnxObjectType]').val();
+	$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=listValue]').val(''):
 	switch ($(this).val()){
 		case "cursor":
 		case "numeric":
 			$(this).closest('.cmd').find('.ValeurMinMax').show();
 			$(this).closest('.cmd').find('.ValeurUnite').show();
 			$(this).closest('.cmd').find('.ValeurDefaut').hide();
-			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]').closest('.input-group').parent().show();
+			$(this).closest('.cmd').find('.listValue').hide();
+			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]')
+				.closest('.input-group').parent().show();
 		break;
 		case "other":
 			$(this).closest('.cmd').find('.ValeurDefaut').show();
 			$(this).closest('.cmd').find('.ValeurMinMax').hide();
 			$(this).closest('.cmd').find('.ValeurUnite').hide();
-			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]').closest('.input-group').parent().hide();
+			$(this).closest('.cmd').find('.listValue').hide();
+			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]')
+				.closest('.input-group').parent().hide();
 		break;	
 		case "binary":
 			$(this).closest('.cmd').find('.ValeurMinMax').hide();
 			$(this).closest('.cmd').find('.ValeurUnite').hide();
 			$(this).closest('.cmd').find('.ValeurDefaut').hide();
+			$(this).closest('.cmd').find('.listValue').hide();
 			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]')
 				.closest('.input-group').parent().show();
+		break;
+		case "select":
+			$(this).closest('.cmd').find('.ValeurMinMax').hide();
+			$(this).closest('.cmd').find('.ValeurUnite').hide();
+			$(this).closest('.cmd').find('.ValeurDefaut').hide();
+			$(this).closest('.cmd').find('.listValue').show();
+			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=listValue]').val(DptListSelect(Dpt)):
+			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]')
+				.closest('.input-group').parent().hide();
 		break;
 		default:
 			$(this).closest('.cmd').find('.ValeurDefaut').hide();
 			$(this).closest('.cmd').find('.ValeurMinMax').hide();
 			$(this).closest('.cmd').find('.ValeurUnite').hide();
+			$(this).closest('.cmd').find('.listValue').hide();
 			$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=inverse]')
 				.closest('.input-group').parent().hide();
 		break;
@@ -379,6 +396,21 @@ function DptOption(Dpt,div){
 			}
 		});
 	});
+}
+function DptListSelect(Dpt){
+  	var DptListSelect='';
+	$.each(AllDpt, function(DptKeyGroup, DptValueGroup){
+		$.each(DptValueGroup, function(DptKey, DptValue){
+			if (DptKey==Dpt){
+				$.each(DptValue.Valeurs, function(keyValeurs, Valeurs){
+					if (DptListSelect !="")
+						DptListSelect.="|";
+					DptListSelect.=keyValeurs;
+				});
+			}
+		});
+	});
+	return DptListSelect;
 }
 function DptValue(Dpt){
   	var DptValues=$('<div>');
