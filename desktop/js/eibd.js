6ac18800fac495d4162e04dd9d49bfa66a2d7bff
Halbout Michael <michaelhalbout@gmail.com>
2017-10-17 13:29:43 +0200

    Update eibd.js

--- desktop/js/eibd.js
+++ desktop/js/eibd.js
@@ -112,6 +112,7 @@ $('.eqLogicAction[data-action=addByTemplate]').on('click', function () {
 						var eqLogic=template[$('.EqLogicTemplateAttr[data-l1key=template]').value()];
 						eqLogic.name=$('.EqLogicTemplateAttr[data-l1key=name]').value();
 						eqLogic.logicalId=$('.EqLogicTemplateAttr[data-l1key=logicalId]').value();
+						eqLogic.configuration.typeTemplate=$('.EqLogicTemplateAttr[data-l1key=template]').value();
 						$.each(eqLogic.cmd,function(index, value){
 							eqLogic.cmd[index].logicalId=$('.CmdEqLogicTemplateAttr[data-l1key='+index+']').value();
 							if (typeof(eqLogic.cmd[index].value) !== 'undefined')
