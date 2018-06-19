ae7fd9ee465ea68b14e856c837a51d94c139687a
Halbout Michael <michaelhalbout@gmail.com>
2018-06-19 13:33:41 +0200

    Update eibd.class.php

--- core/class/eibd.class.php
+++ core/class/eibd.class.php
@@ -952,7 +952,7 @@ class _BusMonitorTraitement /*extends Thread*/{
 		}
 		$monitor['datetime'] = date('d-m-Y H:i:s');
 		event::add('eibd::monitor', json_encode($monitor));
-		exit();
+		//exit();
 	}
 	private function formatiaddr ($addr){
 		return sprintf ("%d.%d.%d", ($addr >> 12) & 0x0f, ($addr >> 8) & 0x0f, $addr & 0xff);
