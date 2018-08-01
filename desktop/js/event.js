a78cc076ec86a1e3a6463c1f600fd4e5651a8955
Halbout Michael <michaelhalbout@gmail.com>
2018-08-01 10:20:21 +0200

    Create event.js

---
+++ desktop/js/event.js
@@ -0,0 +1,31 @@
+$('body').off().on('eibd::GadInconnue', function (_event,_options) {
+	var monitors=jQuery.parseJSON(_options);
+	bootbox.dialog({
+		title: "{{Gad inconnue détecté}}",
+		height: "800px",
+		width: "auto",
+		message: $('<div>').load('index.php?v=d&modal=eibd.gadInconnue&plugin=eibd&type=eibd&SelectAddr='+SelectAddr+'&SelectDpt='+SelectDpt+'&param'),
+		buttons: {
+			"Annuler": {
+				className: "btn-default",
+				callback: function () {
+					//el.atCaret('insert', result.human);
+				}
+			},
+			"Ajouter a un Template": {
+				className: "btn-default",
+				callback: function () {
+					//el.atCaret('insert', result.human);
+				}
+			},
+			success: {
+				label: "Ajouter a un equipement",
+				className: "btn-primary",
+				callback: function () {
+					input.closest('.modal-body').find('.EqLogicTemplateAttr[data-l1key=logicalId]').val(SelectAddr);	
+					input.val(SelectGad);
+				}
+			},
+		}
+	});
+}
