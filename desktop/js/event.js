efb30a993da24d1321df323103b9ed2bea73eaa0
Halbout Michael <michaelhalbout@gmail.com>
2018-08-02 10:04:51 +0200

    Update event.js

--- desktop/js/event.js
+++ desktop/js/event.js
@@ -1,14 +1,5 @@
 $('body').off().on('eibd::GadInconnue', function (_event,_options) {
-	var value=jQuery.parseJSON(_options);
-	var Html= $('<div>').load('index.php?v=d&modal=eibd.gadInconnue&plugin=eibd&type=eibd');
-	if (typeof(value.DeviceName) !== 'undefined') 
-		Html.find('.equipement').text(value.DeviceName);
-	Html.find('.source').text(value.AdressePhysique);
-	if (typeof(value.cmdName) !== 'undefined') 
-		Html.find('.cmd').text(value.cmdName);
-	Html.find('.destination').text(value.AdresseGroupe);
-	Html.find('.dpt').text(value.DataPointType);
-	Html.find('.valeur').text(value.valeur);
+	var Html= $('<div>').load('index.php?v=d&modal=eibd.gadInconnue&plugin=eibd&type=eibd&newGadOption='+_options);
 	bootbox.dialog({
 		title: "{{Gad inconnue détecté}}",
 		height: "800px",
