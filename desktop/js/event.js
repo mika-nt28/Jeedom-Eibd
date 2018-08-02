0717aa2d9404a86f20e327e24d35f39656179a72
Halbout Michael <michaelhalbout@gmail.com>
2018-08-02 12:38:20 +0200

    Update event.js

--- desktop/js/event.js
+++ desktop/js/event.js
@@ -1,25 +1,27 @@
 $('body').off().on('eibd::GadInconnue', function (_event,_options) {
-	var Html= $('<div>').load('index.php?v=d&modal=eibd.gadInconnue&plugin=eibd&type=eibd');
-	bootbox.dialog({
-		title: "{{Gad inconnue détecté}}",
-		height: "800px",
-		width: "auto",
-		message: Html,
-		buttons: {
-			"Annuler": {
-				className: "btn-default",
-				callback: function () {
-					//el.atCaret('insert', result.human);
-				}
-			},
-			success: {
-				label: "Ajouter a un equipement",
-				className: "btn-primary",
-				callback: function () {
-					//input.closest('.modal-body').find('.EqLogicTemplateAttr[data-l1key=logicalId]').val(SelectAddr);	
-					//input.val(SelectGad);
-				}
-			},
-		}
-	});
+	var value=jQuery.parseJSON(newGadOption);
+  	$.load('index.php?v=d&modal=eibd.gadInconnue&plugin=eibd&type=eibd', function() {
+		bootbox.dialog({
+			title: "{{Gad inconnue détecté}}",
+			height: "800px",
+			width: "auto",
+			message: Html,
+			buttons: {
+				"Annuler": {
+					className: "btn-default",
+					callback: function () {
+						//el.atCaret('insert', result.human);
+					}
+				},
+				success: {
+					label: "Ajouter a un equipement",
+					className: "btn-primary",
+					callback: function () {
+						//input.closest('.modal-body').find('.EqLogicTemplateAttr[data-l1key=logicalId]').val(SelectAddr);	
+						//input.val(SelectGad);
+					}
+				},
+			}
+		});
+	}
 });
