ec5ab13dd313f9069f269a3be72c11331793a722
Halbout Michael <michaelhalbout@gmail.com>
2018-09-24 11:34:48 +0200

    Update eibd.class.php

--- core/class/eibd.class.php
+++ core/class/eibd.class.php
@@ -934,9 +934,9 @@ class _BusMonitorTraitement /*extends Thread*/{
 			$monitor['data']='0x '.$this->Data;
 		$commandes=cmd::byLogicalId($this->AdrGroup);
 		if(count($commandes)>0){
-			if($Commande->getEqType_name() !='eibd')
-				continue;
 			foreach($commandes as $Commande){
+				if($Commande->getEqType_name() !='eibd')
+					continue;
 				$monitor['valeur']=trim($Commande->UpdateCommande($this->Mode,$this->Data));
 				$monitor['cmdJeedom']= $Commande->getHumanName();
 				$monitor['DataPointType']=$Commande->getConfiguration('KnxObjectType');
