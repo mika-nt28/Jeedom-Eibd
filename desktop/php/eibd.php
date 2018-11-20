09fbb164b14951f4e36fbbec5abfc6cc9cb547a0
Halbout Michael <michaelhalbout@gmail.com>
2018-11-20 10:18:28 +0100

    Update eibd.php

--- desktop/php/eibd.php
+++ desktop/php/eibd.php
@@ -12,14 +12,20 @@ $eqLogics = eqLogic::byType('eibd');
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default btn-sm tooltips BusMoniteur" title="Bus Moniteur" style="width : 30%">
-			<i class="fa fa-archive"></i>
+                <a class="btn btn-default btn-sm tooltips BusMoniteur" title="Bus Moniteur" style="width : 50%">
+			<i class="fa fa-archive"> {{Moniteur}}</i>
 		</a>
-                <a class="btn btn-default btn-sm tooltips Ets4Parser " title="Export ETS4" style="width : 30%">
+                <a class="btn btn-default btn-sm tooltips GadInconue" title="Bus Moniteur" style="width : 50%">
+			<i class="icon techno-fleches"> {{Inconnu}}</i>
+		</a>
+                <!--a class="btn btn-default btn-sm tooltips Ets4Parser " title="Export ETS4" style="width : 30%">
 			<i class="fa fa-spinner"></i>
-		</a>
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
+		</a-->
+                <a class="btn btn-default eqLogicAction" style="width : 50%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
 			<i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
+                <li class="filter" style="margin-bottom: 5px;">
+                <a class="btn btn-default eqLogicAction" style="width : 50%;margin-top : 5px;margin-bottom: 5px;" data-action="addByTemplate">
+			<i class="fa fa-plus-circle"></i> {{Ajouter un Template}}</a>
                 <li class="filter" style="margin-bottom: 5px;">
 			<input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/>
 		</li>
@@ -85,12 +91,12 @@ $eqLogics = eqLogic::byType('eibd');
 				</center>
 				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Importer les commandes inconnue}}</center></span>
 			</div>
-			<div class="cursor Ets4Parser" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+			<!--div class="cursor Ets4Parser" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
 					<i class="fa fa-spinner" style="font-size : 5em;color:#767676;"></i>
 				</center>
 				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Import ETS4}}</center></span>
-			</div>
+			</div-->
 			<div class="cursor log" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
 					<i class="fa fa-heartbeat" style="font-size : 5em;color:#767676;"></i>
