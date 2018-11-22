e9c112d7e453878f6fbd801fee8169dd35db86eb
Halbout Michael <michaelhalbout@gmail.com>
2018-11-22 09:45:58 +0100

    Update eibd.ajax.php

--- core/ajax/eibd.ajax.php
+++ core/ajax/eibd.ajax.php
@@ -96,11 +96,13 @@ try {
 	if (init('action') == 'EtsParser') {
 		if (isset($_FILES['Knxproj']))
 			ajax::success(knxproj::ParserEtsFile($_FILES['Knxproj']['tmp_name']));
+		ajax::success(false);
 	}
 	if (init('action') == 'getEtsProj') {
 		$filename=dirname(__FILE__) . '/../config/EtsProj.json';
 		if (file_exists($filename))
 			ajax::success(file_get_contents($filename));
+		ajax::success(false);
 	}
   	if (init('action') == 'AppliTemplate') {
 		$EqLogic=eqLogic::byId(init('id'));
