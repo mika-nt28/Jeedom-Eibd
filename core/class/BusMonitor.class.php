97540f6b4fcf11380e319d394c1b02b7c5c9f665
Halbout Michael <michaelhalbout@gmail.com>
2018-11-22 14:21:40 +0100

    Update BusMonitor.class.php

--- core/class/BusMonitor.class.php
+++ core/class/BusMonitor.class.php
@@ -87,7 +87,7 @@ class BusMonitorTraitement /*extends Thread*/{
 	private function recursive_array_search($needle,$haystack) {
 		foreach($haystack as $key=>$value) {
 			$current_key=$key;
-			if($needle===$value OR (is_array($value) && recursive_array_search($needle,$value) !== false)) 
+			if($needle===$value OR (is_array($value) && $this->recursive_array_search($needle,$value) !== false)) 
 				return $current_key;
 		}
 		return false;
