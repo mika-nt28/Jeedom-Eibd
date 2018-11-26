9f6f17c5eb2e64e8b2ee8d5ef46836f7a1bb0cd8
Halbout Michael <michaelhalbout@gmail.com>
2018-11-26 12:58:39 +0100

    Update eibd.EtsParser.php

--- desktop/modal/eibd.EtsParser.php
+++ desktop/modal/eibd.EtsParser.php
@@ -3,7 +3,7 @@ if (!isConnect('admin')) {
     throw new Exception('401 Unauthorized');
 }
 ?>
-<div class="row">  	
+<div class="row EtsParserDiv">  	
 	<div class="col-md-12"> 
 		<p>Cette option du plugin permet de configurer automatiquement votre installation sous Jeedom.</p>
 		<p><b>Attention :</b></p>
@@ -11,7 +11,24 @@ if (!isConnect('admin')) {
 		<p>Il est possible que tous les possibilitées de programation ne soit pas pris en compte, il est impératif de verifier et compléter la configuration a la fin de lexecution</p>
 		<form class="form-horizontal" onsubmit="return false;"> 
 			<div class="form-group"> 
-				<label class="col-md-4 control-label">{{Nom}}</label> 
+				<label class="col-md-4 control-label">{{Créer les equipements repondant a la typo}}</label> 
+				<input type="checkbox" class="form-control EtsParseParameter" data-l1key="createEqLogic"/>
+				<?
+				for($loop = 0; $loop < config::byKey('level','eibd'); $loop++){
+					echo '<select class="form-control EtsParseParameter" data-l1key="'.$loop.'">';
+					echo '	<option value="object">{{Objet}}</option>';
+					echo '	<option value="function">{{Fonction}}</option>';
+					echo '	<option value="cmd">{{Commande}}</option>';
+					echo '</select>';
+				}
+				?>
+			</div> 
+			<div class="form-group"> 
+				<label class="col-md-4 control-label">{{Créer les objet}}</label> 
+				<input type="checkbox" class="form-control EtsParseParameter" data-l1key="createObjet"/>
+			</div> 
+			<div class="form-group"> 
+				<label class="col-md-4 control-label">{{Importer votre projet}}</label> 
 				<input type="file" name="Knxproj" id="Knxproj" data-url="plugins/eibd/core/ajax/eibd.ajax.php?action=EtsParser" placeholder="{{Ficher export ETS}}" class="form-control input-md"/>
 				<div class="col-md-4"></div> 
 			</div> 
@@ -21,6 +38,9 @@ if (!isConnect('admin')) {
 </div>
 <script>
 	$('#Knxproj').fileupload({
+		data:{
+			option: $('body .EtsParserDiv').getValues('.EtsParseParameter'),
+		},
 		dataType: 'json',
 		replaceFileInput: false,
 		//done: function (data) {
