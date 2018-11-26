4309a94c3abb516d52750c8fe37e3aed8934e7e8
Halbout Michael <michaelhalbout@gmail.com>
2018-11-26 13:36:39 +0100

    Update eibd.EtsParser.php

--- desktop/modal/eibd.EtsParser.php
+++ desktop/modal/eibd.EtsParser.php
@@ -12,10 +12,10 @@ if (!isConnect('admin')) {
 		<form class="form-horizontal" onsubmit="return false;"> 
 			<div class="form-group"> 
 				<label class="col-md-4 control-label">{{Créer les equipements repondant a la typo}}</label> 
-				<input type="checkbox" class="form-control EtsParseParameter" data-l1key="createEqLogic"/>
+				<input type="checkbox" class="EtsParseParameter" data-l1key="createEqLogic"/>
 				<?php
 				for($loop = 0; $loop < config::byKey('level','eibd'); $loop++){
-					echo '<select class="form-control EtsParseParameter" data-l1key="'.$loop.'">';
+					echo '<select class=" EtsParseParameter" data-l1key="'.$loop.'">';
 					echo '	<option value="object">{{Objet}}</option>';
 					echo '	<option value="function">{{Fonction}}</option>';
 					echo '	<option value="cmd">{{Commande}}</option>';
@@ -25,16 +25,13 @@ if (!isConnect('admin')) {
 			</div> 
 			<div class="form-group"> 
 				<label class="col-md-4 control-label">{{Créer les objet}}</label> 
-				<input type="checkbox" class="form-control EtsParseParameter" data-l1key="createObjet"/>
+				<input type="checkbox" class="EtsParseParameter" data-l1key="createObjet"/>
 			</div> 
 			<div class="form-group"> 
 				<label class="col-md-4 control-label">{{Importer votre projet}}</label> 
 				<input type="file" name="Knxproj" id="Knxproj" data-url="plugins/eibd/core/ajax/eibd.ajax.php?action=EtsParser" placeholder="{{Ficher export ETS}}" class="form-control input-md"/>
-				<input type="file" name="Knxproj" id="Knxproj" data-url="plugins/eibd/core/ajax/eibd.ajax.php?action=EtsParser" placeholder="{{Ficher export ETS}}" class="form-control input-md"/>
-				<div class="col-md-4"></div> 
 			</div> 
 		</form> 
-		<pre class="EtsImportData"></pre>
 	</div>  
 </div>
 <script>
@@ -48,7 +45,6 @@ if (!isConnect('admin')) {
 				return;
 			}
 			$('#div_alert').showAlert({message: "Import ETS complet.</br>Vous pouvez commancer la configuration des equipements", level: 'success'});
-			$('.EtsImportData').append(data.result);
 		}
 	});
 </script>
