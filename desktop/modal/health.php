5ac5f17de696c9984e5b2decb09df9a5e3b8c4a0
Halbout Michael <michaelhalbout@gmail.com>
2019-01-24 15:18:22 +0100

    Update health.php

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -5,52 +5,50 @@ if (!isConnect('admin')) {
 $eqLogics = eibd::byType('eibd');
 ?>
 <div class="row">
-	<form class="form-horizontal" onsubmit="return false;" style="height: 500px;">
-		<table class="table table-condensed tablesorter" id="table_healthEibd">
-			<thead style="height: 40%;">
-				<tr>
-					<th></th>
-					<th>{{ID}}</th>
-					<th>{{Module}}</th>
-					<th>{{Adresse Physique}}</th>
-					<th>{{Statut}}</th>
-					<th>{{Batterie}}</th>
-					<th>{{Dernière communication}}</th>
-					<th>{{Date création}}</th>
-				</tr>
-			</thead>
-			<tbody style="height: 40%;overflow: auto;">
-			<?php
-				foreach ($eqLogics as $eqLogic) {
-					$file='plugins/eibd/core/config/devices/'.$eqLogic->getConfiguration('typeTemplate').'.png';
-					if(file_exists($file))
-						echo '<td><img src="'.$file.'" height="55"  /></td>';
-					else
-						echo '<td><img src="plugins/eibd/plugin_info/eibd_icon.png" height="55" /></td>';
-					echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getId() . '</span></td>';
-					echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getHumanName() . '</span></td>';
-					echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getLogicalId() . '</span></td>';
-					$status = '<span class="label label-success" style="font-size : 1em;cursor:default;">{{OK}}</span>';
-					if ($eqLogic->getStatus('state') == 'nok') {
-						$status = '<span class="label label-danger" style="font-size : 1em;cursor:default;">{{NOK}}</span>';
-					}
-					echo '<td>' . $status . '</td>';
-					$battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
-					if ($eqLogic->getStatus('battery') < 20 && $eqLogic->getStatus('battery') != '') {
-						$battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
-					} elseif ($eqLogic->getStatus('battery') < 60 && $eqLogic->getStatus('battery') != '') {
-						$battery_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
-					} elseif ($eqLogic->getStatus('battery') > 60 && $eqLogic->getStatus('battery') != '') {
-						$battery_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
-					} else {
-						$battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
-					}
-					echo '<td>' . $battery_status . '</td>';
-					echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
-					echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
+	<table class="table table-condensed tablesorter" id="table_healthEibd" style="width:300px;display:block">
+		<thead style="height: 40%;">
+			<tr>
+				<th></th>
+				<th>{{ID}}</th>
+				<th>{{Module}}</th>
+				<th>{{Adresse Physique}}</th>
+				<th>{{Statut}}</th>
+				<th>{{Batterie}}</th>
+				<th>{{Dernière communication}}</th>
+				<th>{{Date création}}</th>
+			</tr>
+		</thead>
+		<tbody style='height:50px;display:block;overflow:scroll'>
+		<?php
+			foreach ($eqLogics as $eqLogic) {
+				$file='plugins/eibd/core/config/devices/'.$eqLogic->getConfiguration('typeTemplate').'.png';
+				if(file_exists($file))
+					echo '<td><img src="'.$file.'" height="55"  /></td>';
+				else
+					echo '<td><img src="plugins/eibd/plugin_info/eibd_icon.png" height="55" /></td>';
+				echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getId() . '</span></td>';
+				echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getHumanName() . '</span></td>';
+				echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getLogicalId() . '</span></td>';
+				$status = '<span class="label label-success" style="font-size : 1em;cursor:default;">{{OK}}</span>';
+				if ($eqLogic->getStatus('state') == 'nok') {
+					$status = '<span class="label label-danger" style="font-size : 1em;cursor:default;">{{NOK}}</span>';
 				}
-			?>
-			</tbody>
-		</table>
-	</form>
+				echo '<td>' . $status . '</td>';
+				$battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
+				if ($eqLogic->getStatus('battery') < 20 && $eqLogic->getStatus('battery') != '') {
+					$battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+				} elseif ($eqLogic->getStatus('battery') < 60 && $eqLogic->getStatus('battery') != '') {
+					$battery_status = '<span class="label label-warning" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+				} elseif ($eqLogic->getStatus('battery') > 60 && $eqLogic->getStatus('battery') != '') {
+					$battery_status = '<span class="label label-success" style="font-size : 1em;">' . $eqLogic->getStatus('battery') . '%</span>';
+				} else {
+					$battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fa fa-plug"></i></span>';
+				}
+				echo '<td>' . $battery_status . '</td>';
+				echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
+				echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
+			}
+		?>
+		</tbody>
+	</table>
 </div>
