d01ff6fe8a316074b664daa7424c069e1eb415f8
Halbout Michael <michaelhalbout@gmail.com>
2019-05-27 12:30:23 +0200

    Update pre_install.php

--- plugin_info/pre_install.php
+++ plugin_info/pre_install.php
@@ -2,6 +2,6 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function eibd_pre_update() {
-  exec("sudo rm -R ". dirname(__FILE__)."/../../eibd");
+  exec("sudo rm -R ". dirname(__FILE__)."/../core/config/devices/");
 }
 ?>
