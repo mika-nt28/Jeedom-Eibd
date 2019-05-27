a0383eeab2f256198ecc7d6cad0dc9947c38a577
Halbout Michael <michaelhalbout@gmail.com>
2019-05-27 12:22:15 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -7,6 +7,23 @@ function eibd_update() {
 	log::add('eibd','debug','Lancement du scripte de mise a  jours'); 
 	while(is_object($listener=listener::byClassAndFunction('eibd', 'TransmitValue')))
 		$listener->remove();
+	foreach(eqLogic::byType('eibd') as $eqLogic){
+		switch($eqLogic->getConfiguration('typeTemplate')){
+			case 'thermostat_ver':
+				$eqLogic->setConfiguration('typeTemplate','thermostat_verrou');
+			break;
+			case 'dimmerRGB':
+				$eqLogic->setConfiguration('typeTemplate','light_dimmer_rgb');
+			break;
+			case 'RGBW':
+				$eqLogic->setConfiguration('typeTemplate','light_dimmer_rgb');
+			break;
+			case 'dimmer':
+				$eqLogic->setConfiguration('typeTemplate','light_dimmer');
+			break;
+		}
+		$eqLogic->save();
+	}
 }
 function eibd_remove() {
 	while(is_object($listener=listener::byClassAndFunction('eibd', 'TransmitValue')))
