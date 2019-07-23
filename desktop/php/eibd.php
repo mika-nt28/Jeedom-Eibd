02d26c0362e2e3cdacaeab817c5d5e4c66d2d314
Halbout Michael <michaelhalbout@gmail.com>
2019-07-23 10:18:38 +0200

    Update eibd.php

--- desktop/php/eibd.php
+++ desktop/php/eibd.php
@@ -162,7 +162,7 @@
 			</div>
 			<div role="tabpanel" class="tab-pane" id="commandtab">
 				<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a>
-				<a class="btn btn-default btn-sm Template pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Template}}</a><br/><br/>
+				<a class="btn btn-primary btn-sm Template pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Template}}</a><br/><br/>
 				<table id="table_cmd" class="table table-bordered table-condensed">
 					<thead>
 						<tr>
