2cf82ef8de807047af5fc5d0579fa513bdd0911c
Halbout Michael <michaelhalbout@gmail.com>
2019-10-21 11:09:42 +0200

    Update ETSparse.js

--- desktop/js/ETSparse.js
+++ desktop/js/ETSparse.js
@@ -1,24 +1,23 @@
-function ImportEts(){
-  return $('<form class="form-horizontal" onsubmit="return false;">  ')
-			.append($('<div class="form-group">')
-				.append($('<label class="col-md-4 control-label">')
-          .append('{{Type de fichier}}')
-					.append($('<sup>')
-						.append($('<i class="fa fa-question-circle tooltips" title="{{Sélectioner le type de fichier}}">'))))
-				.append($('<select class=" EtsParseParameter" data-l1key="ProjetType">')
-					.append($('<option value="ETS">')
-            .append('{{ETS}}'))
-					.append($('<option value="TX100">')
-            .append('{{TX100}}'))));
-}
+function ImportEts(merge){
+	var html $('<form class="form-horizontal" onsubmit="return false;">  ')
+		.append($('<div class="form-group">')
+			.append($('<label class="col-md-4 control-label">')
+				.append('{{Type de fichier}}')
+				.append($('<sup>')
+					.append($('<i class="fa fa-question-circle tooltips" title="{{Sélectioner le type de fichier}}">'))))
+			.append($('<select class=" EtsParseParameter" data-l1key="ProjetType">')
+				.append($('<option value="ETS">')
+					.append('{{ETS}}'))
+				.append($('<option value="TX100">')
+					.append('{{TX100}}'))));
+
 	bootbox.dialog({
 		title: "{{Importer votre projet KNX}}",
-    message: html,
+		message: html,
 		buttons: {
 			"Annuler": {
 				className: "btn-default",
 				callback: function () {
-					//el.atCaret('insert', result.human);
 				}
 			},
 			success: {
@@ -31,24 +30,39 @@ function ImportEts(){
 						data:
 						{
 							action: 'AnalyseEtsProj',
-							option: $('body .EtsParserDiv').getValues('.EtsParseParameter')
+							merge: merge,
+							ProjetType: $('.EtsParseParameter[data-l1key=ProjetType]').val()
 						},
 						dataType: 'json',
 						global: true,
 						error: function(request, status, error) {},
 						success: function(data) {
-							if($('body .EtsParserDiv .EtsParseParameter[data-l1key=createEqLogic]').is(':checked')){
-								window.location.reload();
-							}else{
-								//UpdateDeviceTable(data.result.Devices);
-								CreateArboressance(data.result.Devices,$('.MyDeviceGroup'),true);
-								CreateArboressance(data.result.GAD,$('.MyAdressGroup'),true);
-								CreateArboressance(data.result.Locations,$('.MyLocationsGroup'),true);
-							}
+							bootbox.confirm({
+								message: "This is a confirm with custom button text and color! Do you like it?",
+								buttons: {
+									confirm: {
+										label: '{{Oui}}',
+										className: 'btn-success'
+									},
+									cancel: {
+										label: '{{Non}}',
+										className: 'btn-danger'
+									}
+								},
+								callback: function (result) {
+									if(result){
+										ImportEts(true);
+									}else{
+										CreateArboressance(data.result.Devices,$('.MyDeviceGroup'),true);
+										CreateArboressance(data.result.GAD,$('.MyAdressGroup'),true);
+										CreateArboressance(data.result.Locations,$('.MyLocationsGroup'),true);
+									}
+								}
+							});
 						}
 					});
 				}
 			},
 		}
 	});
-});
+}
