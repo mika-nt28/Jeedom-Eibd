26f31d756641cfa43452ea1d5249ee715917d374
Halbout Michael <michaelhalbout@gmail.com>
2019-10-23 09:40:37 +0200

    Update autoCreate.js

--- desktop/js/autoCreate.js
+++ desktop/js/autoCreate.js
@@ -1,5 +1,5 @@
 function getLevelSelect(){
-	var html='';
+	var html=$('<div class="col-sm-9 control-label">');
 	for(var loop = 0; loop < GadLevel; loop++){
 		html.append($('<select class=" autoCreateParameter" data-l1key="'+loop+'">')
 			.append($('<option value="object">')
@@ -13,29 +13,32 @@ function getLevelSelect(){
 function autoCreate(){
 	var html = $('<form class="autoCreate form-horizontal" onsubmit="return false;">');
   	html.append($('<div class="form-group">') 
-		.append($('<label class="col-md-4 control-label">') 
+		.append($('<label class="col-sm-4 control-label">') 
         		.append('{{Créer les objets}}') 
 			.append($('<sup>') 
 				.append($('<i class="fa fa-question-circle tooltips" title="{{Cette option autorise le parser a créer automatiquement les objets trouvés selon la definition des level defini precedement dans l\'arboresance de groupe}}" >'))))
-		.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createObjet"/>')));
+		.append($('<div class="col-sm-9 control-label">') 
+			.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createObjet"/>'))));
 	html.append($('<div class="form-group">') 
-		.append($('<label class="col-md-4 control-label">') 
+		.append($('<label class="col-sm-4 control-label">') 
       			.append('{{Créer les equipements}}') 
 			.append($('<sup>') 
 				.append($('<i class="fa fa-question-circle tooltips" title="{{Cette option autorise le parser a créer automatiquement les equipements trouvés selon la definition des level defini precedement dans l\'arboresance de groupe}}">'))))
-		.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createEqLogic"/>')));
+		.append($('<div class="col-sm-9 control-label">') 
+			.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createEqLogic"/>'))));
 	html.append($('<div class="form-group withCreate">') 
-		.append($('<label class="col-md-4 control-label">') 
+		.append($('<label class="col-sm-4 control-label">') 
 		        .append('{{Arborescence des groupes}}') 
 			.append($('<sup>') 
 				.append($('<i class="fa fa-question-circle tooltips" title="{{La définition de l\'arboressance de groupe permet au parser de connaitre ou se situe le nom a prendre pour la creation automatique des objets ou des equipemnt}}">'))))
 			.append(getLevelSelect).hide());
 	html.append($('<div class="form-group withCreateEqLogic">') 
-		.append($('<label class="col-md-4 control-label">')
+		.append($('<label class="col-sm-4 control-label">')
        			.append('{{Uniquement correspondant a un Template}}')
 			.append($('<sup>')
 				.append($('<i class="fa fa-question-circle tooltips" title="{{Cette option permet de filtrer la creation d\'equipement a ceux qui corresponde a un Template (Nom du Template et des commandes}}">'))))
-		.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createTemplate"/>')).hide());
+		.append($('<div class="col-sm-9 control-label">') 
+			.append($('<input type="checkbox" class="autoCreateParameter" data-l1key="createTemplate"/>'))).hide());
 bootbox.dialog({
 		title: "{{Creation automatique des equipements KNX}}",
 		message: html,
