141c9769f6adb0fed405dbc33369f152f71ebe98
Halbout Michael <michaelhalbout@gmail.com>
2019-12-13 11:02:16 +0100

    Update eibd.php

--- desktop/php/eibd.php
+++ desktop/php/eibd.php
@@ -98,7 +98,10 @@
 								echo '<td><div style="display: none;">'.$eqLogic->getConfiguration('typeTemplate').'</div><img src="'.$file.'" height="45"  /></td>';
 							else
 								echo '<td><img src="plugins/eibd/plugin_info/eibd_icon.png" height="45" /></td>';
-							echo '<td class="eqLogicDisplayAttr" data-l1key="object">' . $eqLogic->getObject()->getHumanName(true,true) . '</td>';
+							echo '<td class="eqLogicDisplayAttr" data-l1key="object">'
+							if(is_object($eqLogic->getObject()))
+								echo $eqLogic->getObject()->getHumanName(true,true);
+							echo '</td>';
 							echo '<td><span class="label label-info eqLogicDisplayAttr" data-l1key="name">' . $eqLogic->getName() . '</span></td>';
 							echo '<td><span class="label label-info eqLogicDisplayAttr" data-l1key="logicalId"> ' . $eqLogic->getLogicalId() . '</span></td>';
 							echo '<td>';
