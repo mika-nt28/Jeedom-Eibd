6d29744452561eaa2ba815681c222fb955e2fa17
Halbout Michael <michaelhalbout@gmail.com>
2022-04-08 08:36:24 +0200

    Update install-knxd.sh

--- ressources/install-knxd.sh
+++ ressources/install-knxd.sh
@@ -4,7 +4,14 @@ echo "*                                         Remove eibd                     
 echo "*****************************************************************************************************"
 sudo rm -R /usr/local/bin/{eibd,knxtool,group*} /usr/local/lib/lib{eib,pthsem}*.so* /usr/local/include/pth*
 sudo rm -R /var/log/knxd.log
-cd /usr/local/bin/
+echo 10 > /tmp/jeedom_install_in_progress_eibd
+echo "*****************************************************************************************************"
+echo "*                                         Remove knxd                                               *"
+echo "*****************************************************************************************************"
+suso systemctl stop knxd.socket
+suso systemctl stop knxd
+sudo apt-get autoremove -y knxd
+echo 20 > /tmp/jeedom_install_in_progress_eibd
 echo "*****************************************************************************************************"
 echo "*                                      Installation de KnxD                                         *"
 echo "*****************************************************************************************************"
@@ -16,11 +23,14 @@ fi
 sudo mkdir /usr/local/src/knxd
 cd knxd
 sudo git clone https://github.com/knxd/knxd.git
+echo 30 > /tmp/jeedom_install_in_progress_eibd
 cd knxd
 sudo git checkout debian
 sudo dpkg-buildpackage -b -uc -d
+echo 60 > /tmp/jeedom_install_in_progress_eibd
 cd /usr/local/src/knxd
 sudo dpkg -i knxd_*.deb knxd-tools_*.deb
+echo 90 > /tmp/jeedom_install_in_progress_eibd
 sudo usermod -a -G dialout knxd
 echo "*****************************************************************************************************"
 echo "*                                       Installation terminé                                        *"
