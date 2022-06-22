ad4adf422b368a988ac1caefa900c212182d9965
Halbout Michael <michaelhalbout@gmail.com>
2022-06-22 18:35:03 +0200

    Update eibd.log.php

--- desktop/modal/eibd.log.php
+++ desktop/modal/eibd.log.php
@@ -4,13 +4,22 @@ if (!isConnect()) {
 }
 
 ?>
-<div class='Log'></div>
+<table id="table_Log" class="table table-bordered table-condensed tablesorter">
+			<thead>
+				<tr>
+					<th>{{Date}}</th>
+					<th>{{Message}}</th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
 <script>
 getLog();
 function getLog(){
 	$.ajax({
 		type: "POST",
-		timeout:8000, 
+		timeout:8000,
+		async: false, 
 		url: "plugins/eibd/core/ajax/eibd.ajax.php",
 		data: {
 			action: "getLog",
@@ -26,11 +35,29 @@ function getLog(){
 				$('#div_alert').showAlert({message: data.result, level: 'danger'});
 				return;
 			}
-			$('.Log').html(data.result);
+			$('#table_Log tbody').html('');
+			$.each(data.result,function(key, value) {
+				var tr=$("<tr>");
+				tr.append($("<td>").text(timeConverter(value.__REALTIME_TIMESTAMP)));
+				tr.append($("<td>").text(value.MESSAGE));
+				$('#table_Log tbody').append(tr);
+			});	
 			setTimeout(function() {
 				getLog()
-			}, 60000);
+			}, 500);
 		}
 	});	
+}
+function timeConverter(UNIX_timestamp){
+	var a = new Date(UNIX_timestamp);
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	var year = a.getFullYear();
+	var month = months[a.getMonth()];
+	var date = a.getDate();
+	var hour = a.getHours();
+	var min = a.getMinutes();
+	var sec = a.getSeconds();
+	var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	return time;
 }
 </script>
